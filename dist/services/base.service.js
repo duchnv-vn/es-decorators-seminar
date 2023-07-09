8ddae17ef2d7b3e30efaa2f886db658f2c634948
duc-coder <huynhnguyenvietduc@gmail.com>
2023-07-09 16:10:31 +0700

    Delete dist folder

--- dist/services/base.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseService = void 0;
-class BaseService {
-    originFunction() {
-        return "function run success";
-    }
-}
-exports.BaseService = BaseService;
--- dist/services/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.homeService = exports.baseService = void 0;
-const base_service_1 = require("./base.service");
-const home_service_1 = require("./home.service");
-exports.baseService = new base_service_1.BaseService();
-exports.homeService = new home_service_1.HomeService();
